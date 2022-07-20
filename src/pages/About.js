a399bd48a4865db9b50066b9ef051d737d244d8c
RATSENO <onestar1014@gmail.com>
2022-07-20 15:57:58 +0900

    라우터 - 쿼리스트링

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,33 @@
-import {useLocation} from 'react-router-dom'
+import {useLocation, useSearchParams} from 'react-router-dom'
 
 const About = () =>{
-    const location = useLocation();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const detail = searchParams.get('detail');
+    const mode = searchParams.get('mode');
+
+    const onToggleDetail = () => {
+        setSearchParams({
+            mode, 
+            detail : detail === 'true' ? false : true
+        });
+    }
+
+    const onIncreaseMode = () => {
+        const nextMode = mode === null ? 1 : parseInt(mode) + 1;
+        setSearchParams({
+            mode : nextMode,
+            detail
+        });
+    };
 
     return (
         <div>
             <h1>소개</h1>
             <p>리액트</p>
-            <p>쿼리 스트링 : {location.search}</p>
+            <p>detail : {detail}</p>
+            <p>mode : {mode}</p>
+            <button onClick={onToggleDetail}>Toggle detail</button>
+            <button onClick={onIncreaseMode}>mode + 1</button>
         </div>
     )
 };
