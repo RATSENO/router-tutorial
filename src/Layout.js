cbb263ce8a6791da78b77c9fd29e6fc7481bd707
RATSENO <onestar1014@gmail.com>
2022-07-22 10:21:23 +0900

    useNavigate

--- src/Layout.js
+++ src/Layout.js
@@ -1,9 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () =>{
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        //이전 페이지 이동
+        navigate(-1);
+    };
+
+    const goArticles = () => {
+        //articles 경로로 이동
+        navigate('/articles', {replace:true});
+    };
+
     return (
         <div>
-            <header style={{background:'lightgray', padding: 16, fontSize: 24}}>Header</header>
+            <header style={{background:'lightgray', padding: 16, fontSize: 24}}>
+                <button onClick={goBack}>뒤로가기</button>
+                <button onClick={goArticles}>게시글 목록</button>
+            </header>
             <main>
                 <Outlet></Outlet>
             </main>
