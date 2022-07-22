5c2f9108ac80f229b5c2885c9df1fcda9d08f0c8
RATSENO <onestar1014@gmail.com>
2022-07-22 10:49:21 +0900

    Navigate 컴포넌트

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Articles from "./pages/Articles";
 import Article from "./pages/Article";
 import Layout from "./Layout";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
       <Route path="/articles" element={<Articles></Articles>}>
         <Route path=":id" element={<Article></Article>}></Route>
       </Route>
+      <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/mypage" element={<MyPage></MyPage>}></Route>
       <Route path="*" element={<NotFound></NotFound>}></Route>
     </Routes>
   );
